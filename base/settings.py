a011c6cc79566b907e519bee0516a209fac44f59
12moi <12moi>
2022-04-15 14:20:47 +0300

    add hood database

--- base/settings.py
+++ base/settings.py
@@ -84,12 +84,16 @@ WSGI_APPLICATION = 'base.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hood',
+        'USER': 'moringa',
+    'PASSWORD':'12access',
     }
 }
+
 
 
 # Password validation
