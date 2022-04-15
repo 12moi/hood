dbdcc68e74f90b5378a0463a3922a3d38a02db99
12moi <12moi>
2022-04-15 09:51:42 +0300

    adupd forms.py  file to hood app

--- hood/views.py
+++ hood/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+
+
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+
+
+from django.contrib.auth.models import User
+
+
 
 # Create your views here.
+@login_required(login_url='login')
+def index(request):
+    return render(request, 'index.html')
