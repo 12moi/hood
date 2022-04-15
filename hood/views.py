d61da837c995401c47acdb9038c4383639080063
12moi <12moi>
2022-04-15 15:24:31 +0300

    add signup method

--- hood/views.py
+++ hood/views.py
@@ -2,8 +2,12 @@
 
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect, get_object_or_404
+from .forms import SignupForm, BusinessForm
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
+from .models import NeighbourHood, Profile, Business, Post
+from .forms import UpdateProfileForm, NeighbourHoodForm, PostForm
+from django.contrib.auth.models import User
 
 
 from django.contrib.auth.models import User
@@ -14,3 +18,18 @@ from django.contrib.auth.models import User
 @login_required(login_url='login')
 def index(request):
     return render(request, 'index.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = SignupForm()
+    return render(request, 'registration/signup.html', {'form': form})
