17aeab88a6b5d22566fe81720faf0723a0c62a9e
12moi <12moi>
2022-04-15 16:19:58 +0300

    add hood method

--- hood/urls.py
+++ hood/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('register/', views.signup, name='signup'),
+    path('account/', include('django.contrib.auth.urls')),
+    path('all-hoods/', views.hoods, name='hood'),
 ]
